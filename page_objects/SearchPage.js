1755a6cfb086c47ac302b53b9ef2ee6d00f57d6b
unknown <sadorusek@gmail.com>
2019-06-15 16:34:11 +0200

    Added few tests releated do search page

--- page_objects/SearchPage.js
+++ page_objects/SearchPage.js
@@ -3,9 +3,14 @@ module.exports = class SearchPage {
     this.path = 'https://www.amazon.com';
     this.searchInut = 'input[id="twotabsearchtextbox"]';
     this.searchSuggestionQuery = 'dre';
-    this.firstSearchSuggestion = 'div[id="issDiv0"]';
+    this.firstSearchSuggestion = 'div[id="issDiv4"]';
     this.searchResultsHeader = 'span[class="a-color-state a-text-bold"]';
     this.searchQuery = 'Sneakers';
+    this.noResultSearchQuery = 'nooooooooooooooooooooooooooooo';
+    this.errorMessage = 'div[class="a-section a-spacing-base a-spacing-top-medium"]';
     this.searchGoButton = 'input[class="nav-input"][value="Go"]';
+    this.doNotChangeAddressButton = 'div[class="glow-toaster-content"] input[class="a-button-input"][data-action-type="DISMISS"]';
+    this.feedbackSection = 'span[data-component-type="s-feedback-slot"]';
+    this.featuredRecommendationSection = 'div[class="rhf-frame"][style="display: block;"]';
   }
 };
--- tests/search.test.js
+++ tests/search.test.js
@@ -2,15 +2,23 @@ const puppeteer = require('puppeteer');
 const SearchPage = require('../page_objects/SearchPage');
 // eslint-disable-next-line prefer-destructuring
 const getText = require('../lib/helpers').getText;
+// eslint-disable-next-line prefer-destructuring
+const scrollToElement = require('../lib/helpers').scrollToElement;
 
 const searchPage = new SearchPage();
 
 beforeEach(async () => {
   browser = await puppeteer.launch({
-    headless: true,
+    headless: false,
+    timeout: 7000,
+    slowMo: 0,
   });
   page = await browser.newPage();
+  jest.setTimeout(30000);
   await page.goto(searchPage.path);
+  // This code is related to the popup, which is showing dependent on your geolocalization
+  await page.waitForSelector(searchPage.doNotChangeAddressButton);
+  await page.click(searchPage.doNotChangeAddressButton);
 });
 
 afterEach(async () => {
@@ -34,5 +42,25 @@ describe('Search functionality', () => {
     const pageUrl = await page.url();
     expect(pageUrl).toContain(searchPage.searchQuery);
     expect(searchResultsHeaderText).toContain(searchPage.searchQuery);
+  });
+});
+describe('Should show error message', () => {
+  it('Search for not existing product', async () => {
+    await page.type(searchPage.searchInut, searchPage.noResultSearchQuery);
+    await page.click(searchPage.searchGoButton);
+    const searchResultsErrorMessage = await getText(page, searchPage.errorMessage);
+    const pageUrl = await page.url();
+    expect(pageUrl).toContain(searchPage.noResultSearchQuery);
+    expect(searchResultsErrorMessage).toContain(`No results for ${searchPage.noResultSearchQuery}`);
+  });
+});
+
+describe('Testing featured recommendations section', () => {
+  it('Featured recommendations section should be visible after scrolling to the bottom of page', async () => {
+    await page.type(searchPage.searchInut, searchPage.searchQuery);
+    await page.click(searchPage.searchGoButton);
+    await page.waitForSelector(searchPage.feedbackSection);
+    await scrollToElement(page, searchPage.feedbackSection);
+    await page.waitForSelector(searchPage.featuredRecommendationSection);
   });
 });
